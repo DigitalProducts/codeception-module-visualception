c0f7ec2ad47cafe42f9aa7926ea6e8b243973e49
Nils Langner <nils.langner@phmlabs.com>
2014-03-24 15:59:27 +0100

    Added test cases for integration tests

---
+++ test/integration/tests/_bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+// This is global bootstrap for autoloading
+
+include_once __DIR__."/../../../module/VisualCeption.php";
---
+++ test/integration/tests/acceptance/TimeComparisonCest.php
@@ -0,0 +1,14 @@
+<?php
+
+class TimeComparisonCest
+{
+
+    /**
+     * Coparing a div that renders the current time
+     */
+    public function compareTimeString (WebGuy $I, $scenario)
+    {
+        $I->amOnPage("/VisualCeption/time.php");
+        $I->compareScreenshot("the-time", "#thetime");
+    }
+}
