3c995059f5c607a9f4907fbbf2792097fe8ebcdf
Nils Langner <nils.langner@phmlabs.com>
2014-03-26 13:51:39 +0100

    minor test changes

--- test/integration/tests/acceptance/SimpleCept.php
+++ test/integration/tests/acceptance/SimpleCept.php
@@ -2,9 +2,9 @@
 
 $I = new WebGuy($scenario);
 
-        $I->amOnPage("/VisualCeption/seeVisualChanges.php");
-        $I->seeVisualChanges("block", "#theblock");
+$I->amOnPage("/VisualCeption/seeVisualChanges.php");
+$I->seeVisualChanges("SimpleBlock", "#theblock");
 
-        // the test has to be called twice for comparison on the travis server
-        $I->amOnPage("/VisualCeption/seeVisualChanges.php");
-        $I->seeVisualChanges("block", "#theblock");
+// the test has to be called twice for comparison on the travis server
+$I->amOnPage("/VisualCeption/seeVisualChanges.php");
+$I->seeVisualChanges("SimpleBlock", "#theblock");
