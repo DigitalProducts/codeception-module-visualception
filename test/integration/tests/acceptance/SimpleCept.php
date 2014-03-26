5465cb03acce17e6919150a7ec26a5b93f710a97
Nils Langner <nils.langner@phmlabs.com>
2014-03-26 13:59:13 +0100

    added wait in test case

--- test/integration/tests/acceptance/SimpleCept.php
+++ test/integration/tests/acceptance/SimpleCept.php
@@ -6,5 +6,6 @@ $I->amOnPage("/VisualCeption/seeVisualChanges.php");
 $I->seeVisualChanges("SimpleBlock", "#theblock");
 
 // the test has to be called twice for comparison on the travis server
+$I->wait(2);
 $I->amOnPage("/VisualCeption/seeVisualChanges.php");
 $I->seeVisualChanges("SimpleBlock", "#theblock");
