c8c93c8a56e727b1718f431e650b70c1f2113f59
Neubert, Sebastian <neubert.sebastian@guj.de>
2014-10-15 15:47:43 +0200

    remove @TODO comment and remove mark as incomplete

--- test/integration/tests/acceptance/WriteCurrentImageCest.php
+++ test/integration/tests/acceptance/WriteCurrentImageCest.php
@@ -10,13 +10,6 @@ class WriteCurrentImageCest
      */
     public function writeCurrentImageFile(WebGuy $I, $scenario)
     {
-        // #############################################################
-
-        // @TODO: complete the test, if current.* image is written
-        $scenario->incomplete();
-
-        // #############################################################
-
         $I->amOnPage("/VisualCeption/seeVisualChanges.php");
         $I->dontSeeVisualChanges("currentImageIdentifier", "#theblock");
 
