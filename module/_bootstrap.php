aa506263fded36cb4699a26d1dec8559448c448a
Neubert, Sebastian <neubert.sebastian@guj.de>
2015-01-26 15:00:35 +0100

    add new _bootstrap file. workaround to autoloader

---
+++ module/_bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__.'/VisualCeption.php';
+require_once __DIR__.'/VisualCeptionReporter.php';
+require_once __DIR__.'/ImageDeviationException.php';
+require_once __DIR__.'/Storage/Factory.php';
+require_once __DIR__.'/Storage/Storage.php';
+require_once __DIR__.'/Storage/FileStorage.php';
+require_once __DIR__.'/Storage/RemoteStorage.php';
+require_once __DIR__.'/Html/Manipulation.php';
+require_once __DIR__.'/Html/Screenshot.php';
+require_once __DIR__.'/Image/Comparison.php';
+require_once __DIR__.'/Image/ComparisonResult.php';
+require_once __DIR__.'/Report/Reporter.php';
+require_once __DIR__.'/Report/CIReporter.php';
+require_once __DIR__.'/Report/FileReporter.php';
+require_once __DIR__.'/Report/templates/ci.php';
+require_once __DIR__.'/Report/templates/template.php';
+require_once __DIR__.'/Report/templates/ci_template.php';
+
+
