09c89747ae38dfb02080f937a98fc4050db607a8
Nils Langner <nils.langner@phmLabs.com>
2015-03-04 09:42:33 +0100

    moved bootstrap to module

---
+++ module/_bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once __DIR__."/VisualCeption.php";
+include_once __DIR__."/ImageDeviationException.php";
+
+include_once __DIR__."/VisualCeptionReporter.php";
+
+include_once __DIR__."/Storage/Factory.php";
+include_once __DIR__."/Storage/Storage.php";
+include_once __DIR__."/Storage/FileStorage.php";
+include_once __DIR__."/Storage/RemoteStorage.php";
+
+include_once __DIR__."/Html/Manipulation.php";
+include_once __DIR__."/Html/Screenshot.php";
+
+include_once __DIR__."/Image/Comparison.php";
+include_once __DIR__."/Image/ComparisonResult.php";
+
+include_once __DIR__."/Report/Reporter.php";
+include_once __DIR__."/Report/FileReporter.php";
+include_once __DIR__."/Report/CIReporter.php";
--- test/integration/tests/_bootstrap.php
+++ test/integration/tests/_bootstrap.php
@@ -3,22 +3,4 @@
 
 sleep(5);
 
-include_once __DIR__."/../../../module/VisualCeption.php";
-include_once __DIR__."/../../../module/ImageDeviationException.php";
-
-include_once __DIR__."/../../../module/VisualCeptionReporter.php";
-
-include_once __DIR__."/../../../module/Storage/Factory.php";
-include_once __DIR__."/../../../module/Storage/Storage.php";
-include_once __DIR__."/../../../module/Storage/FileStorage.php";
-include_once __DIR__."/../../../module/Storage/RemoteStorage.php";
-
-include_once __DIR__."/../../../module/Html/Manipulation.php";
-include_once __DIR__."/../../../module/Html/Screenshot.php";
-
-include_once __DIR__."/../../../module/Image/Comparison.php";
-include_once __DIR__."/../../../module/Image/ComparisonResult.php";
-
-include_once __DIR__."/../../../module/Report/Reporter.php";
-include_once __DIR__."/../../../module/Report/FileReporter.php";
-include_once __DIR__."/../../../module/Report/CIReporter.php";
+include_once __DIR__."/../../../module/_bootstrap.php";
